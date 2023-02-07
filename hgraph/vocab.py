66c863a4b3904d320959e317439005f61a54b2a7
anamika-yadav99 <54634163+anamika-yadav99@users.noreply.github.com>
2023-02-07 10:12:41 +0530

    Update vocab.py

--- hgraph/vocab.py
+++ hgraph/vocab.py
@@ -21,7 +21,7 @@ class Vocab(object):
 class PairVocab(object):
 
     def __init__(self, smiles_pairs, cuda=True):
-        cls = list(zip(*smiles_pairs))
+        cls = list(zip(*smiles_pairs))[0]
         self.hvocab = sorted( list(set(cls)) )
         self.hmap = {x:i for i,x in enumerate(self.hvocab)}
 
